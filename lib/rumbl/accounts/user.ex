414dd3d3c5a07068b7f0da14cd303af0b4916abf
Dan <danieltongnz@gmail.com>
2017-10-18 11:02:29 +1300

    Create new user views and password changeset
    The user views have been changed to use username instead of email.
    
    The registration_changeset checks password validity, and generates a
    password hash.
    
    The comeonin dep also needed the bcrypt dep, this has been added.

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -17,20 +17,21 @@ defmodule Rumbl.Accounts.User do
   def changeset(%User{} = user, attrs) do
     user
     |> cast(attrs, [:name, :username])
+    |> validate_required(:username)
     |> validate_length(:username, min: 1, max: 20)
   end
 
-  def registration_changeset(model, params) do
-    model
-    |> changeset(params)
-    |> cast(params, ~w(password), [])
+  def registration_changeset(%User{} = user, attrs) do
+    user
+    |> changeset(attrs)
+    |> cast(attrs, ~w(password), [])
     |> validate_length(:password, min: 6, max: 100)
     |> put_pass_hash()
   end
 
   defp put_pass_hash(%Ecto.Changeset{valid?: true,
     changes: %{password: password}} = changeset) do
-      change(changeset, :password_hash, Comeonin.Bcrypt.add_hash(password))
+      put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(password))
   end
   defp put_pass_hash(changeset), do: changeset
 end
